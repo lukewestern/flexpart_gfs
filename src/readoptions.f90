73f6f8b412c4381d9eacf6ef894dd0f4a0a2da81
lucieb92 <lucie.bakels@univie.ac.at>
2021-11-24 19:31:48 +0100

    moved skplin to readoptions, moved sort2 to conv_mod

--- src/readoptions.f90
+++ src/readoptions.f90
@@ -3058,3 +3058,32 @@ subroutine readspecies(id_spec,pos_spec)
 end subroutine readspecies
 
 end module readoptions
+
+subroutine skplin(nlines,iunit)
+  !                    i      i
+  !*****************************************************************************
+  !                                                                            *
+  !   This routine reads nlines from unit iunit and discards them              *
+  !                                                                            *
+  !   Authors: Petra Seibert                                                   *
+  !                                                                            *
+  !   31 Dec 1998                                                              *
+  !                                                                            *
+  !*****************************************************************************
+  !                                                                            *
+  ! Variables:                                                                 *
+  !                                                                            *
+  ! iunit   unit number from which lines are to be skipped                     *
+  ! nlines  number of lines to be skipped                                      *
+  !                                                                            *
+  !*****************************************************************************
+
+  implicit none
+
+  integer :: i,iunit, nlines
+
+  do i=1,nlines
+    read(iunit,*)
+  end do
+
+end subroutine skplin
