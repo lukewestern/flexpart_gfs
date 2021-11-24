5b5314dc76125117128f8d7e0fe6a81d0b33a961
lucieb92 <lucie.bakels@univie.ac.at>
2021-11-24 15:35:28 +0100

    moved dynamic_viscosity to get_settling

--- src/get_settling.f90
+++ src/get_settling.f90
@@ -95,8 +95,6 @@ subroutine get_settling(itime,xt,yt,zt,nsp,settling)
 
   reynolds=dquer(nsp)/1.e6*abs(vsetaver(nsp))/vis_kin
 
-
-
   ! Iteration to determine both Reynolds number and settling velocity
   !******************************************************************
 
@@ -121,6 +119,18 @@ subroutine get_settling(itime,xt,yt,zt,nsp,settling)
     reynolds=dquer(nsp)/1.e6*abs(settling)/vis_kin
     settling_old=settling
   end do
-
-
 end subroutine get_settling
+
+real function viscosity(t)
+  ! Function calculates dynamic viscosity of air (kg/m/s) as function of
+  ! temperature (K) using Sutherland's formula
+  implicit none
+
+  real :: t
+  real,parameter :: c=120.,t_0=291.15,eta_0=1.827e-5
+
+  viscosity=eta_0*(t_0+c)/(t+c)*(t/t_0)**1.5
+
+  return
+
+end function viscosity
